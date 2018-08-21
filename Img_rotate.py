2a20a731cd81ebe6b8b81de0abf2d99186c49b4e
he02753324 <oliveryu@gmail.com>
2018-08-21 15:05:47 +0800

    Update Img_rotate.py

--- Img_rotate.py
+++ Img_rotate.py
@@ -1,22 +1,32 @@
 #This program rotate images and save them
 import readdir
 from PIL import Image
+import random
+def rotate(rootdir,desdir):
+ #讀取folder and files
+ image_list=readdir.read(rootdir)
 
-#讀取folder and files
-image_list=readdir.read(r'C:\Image Anotation\rawImageCut')
 
-
-for i in image_list:
+ for i in image_list:
     count=0
     im=Image.open(i)
-    while(count<2):
-        #旋轉
-        im2=im.rotate(60)
+    
+    #將每張image旋轉，count為張數
+    while(count<10):
+        
+        #旋轉 (angle(隨機浮點數),resample,expand)
+        im2=im.rotate(random.uniform(0,360),0,1)
+        
         #去除.jpg 副檔名
         rResult=i.replace('.jpg','')
-    
+        
+        #將source替換成destination
+        rResult=rResult.replace(rootdir,desdir)
+        
         #儲存  
         count+=1
-        im2.save(rResult+"r"+str(count)+".jpg")
-        
+        im2.save(rResult+"_r_"+str(count)+".jpg")
+ print("Done")
+   
+rotate(r'C:\Image Anotation\rawImageCut',r'C:\Image Anotation\rawImageCut')        
         
