c1e30b8d9c0a4af8826ab09522aecc80e1a4b6db
he02753324 <oliveryu@gmail.com>
2018-08-21 15:06:49 +0800

    Update mkdir.py

--- mkdir.py
+++ mkdir.py
@@ -1,25 +1,18 @@
 #This program reads from excels and create folders recursively
-import xlrd
+import readExcel
 from pathlib import Path
 
-def readExcel():
-    #讀取Excel檔建立workbook
-    book = xlrd.open_workbook('C:\Image Anotation\ProductInfo.xlsx')
-
-    #建立worksheet
-    sheet = book.sheet_by_name('工作表1')
-
-    #將r行0欄的資料存進data list
-    data = [sheet.cell_value(r, 0)  for r in range(sheet.nrows)]
-
-    #將數字以外的資料去除
-    data = [num for num in data if isinstance(num, (int,float))]   
-       
-    return data
-#迴圈建立資料夾(已存在不影響)
-data=readExcel()
-for r in data:
+def mkdir(source,sheet,desdir):
+ datalist=readExcel.readExcel(source,sheet)
+ #迴圈建立資料夾(已存在不影響)
+ #r是資料夾名稱
+ for r in datalist:
+    
   r=int(r) 
-  r=str(r)   
-  path = Path(r"C:\Image Anotation\ProductImage\\"+r) 
+  r=str(r)
+  desdir=desdir+r
+  
+  path = Path(desdir) 
   path.mkdir(exist_ok=True)
+
+mkdir('C:\Image Anotation\ProductInfo.xlsx',"工作表1",r"C:\Image Anotation\rawImageCut\\")
